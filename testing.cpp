cdba808b7637763f456a53e5d7840d9f6300e388
Ayham Zeino <ayham.zeino@gmail.com>
2026-05-06 16:32:43 +0400

    New Program

--- testing.cpp
+++ testing.cpp
@@ -1,7 +1,55 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 int main()
 {
-    
+    int size;
+    bool isStrong = false;
+    cout << "Enter the length of your password: ";
+    cin >> size;
+    cin.ignore();
+    char password[size + 1];
+    cout << "Enter a sequence of " << size << " characters: ";
+    cin.getline(password, size + 1);
+    for (int i = 0; i < size; i++)
+    {
+        if (isupper(password[i]))
+        {
+            isStrong = true;
+            break;
+        }
+    }
+    if (!isStrong)
+    {
+        cout << "Weak";
+        return 0;
+    }
+    isStrong = false;
+    for (int i = 0; i < size; i++)
+    {
+        if (islower(password[i]))
+        {
+            isStrong = true;
+            break;
+        }
+    }
+    if (!isStrong)
+    {
+        cout << "Weak";
+        return 0;
+    }
+    isStrong = false;
+    for (int i = 0; i < size; i++)
+    {
+        if (password[i] == '!' || password[i] == '@' || password[i] == '&')
+        {
+            isStrong = true;
+            break;
+        }
+    }
+    if (isStrong)
+        cout << "Strong";
+    else
+        cout << "Weak";
     return 0;
 }
